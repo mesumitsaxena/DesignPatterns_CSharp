851956410b232802345392576c288c6d68b99c50
sumit66saxena <sumit66saxena@gmail.com>
2022-06-24 19:19:47 +0530

    Few additions

--- Creational/Singleton/SingletonThreadSafe.cs
+++ Creational/Singleton/SingletonThreadSafe.cs
@@ -8,6 +8,8 @@ namespace DesignPatterns_CSharp.Creational.Singleton
 {
     internal class SingletonThreadSafe
     {
+        //we are async keyword, by which each thread will come in this method one by one, so there will no
+        // issue with thread creating multiple objects
         // static instance reference which can be accessed within class
         private static SingletonThreadSafe instance = null;
         //Restricting class to create instances
@@ -27,4 +29,8 @@ namespace DesignPatterns_CSharp.Creational.Singleton
             return instance;
         }
     }
+    // Issue with this is we are async keyword on the method it self so whenever multiple thread comes to this method.
+    // they will execute one by one.
+    // By this our performance will reduce and there is no point of having multithreading if they are executing one by one.
+  
 }
